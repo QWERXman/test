cfb4955af1325adef28e47c8adc27ccb181aea1a
qwerx <myEmail@example.com>
2018-10-08 13:21:20 +0300

    c

--- hello.java
+++ hello.java
@@ -2,7 +2,7 @@ public class hello {
   public static void main(String[] args) {
     System.out.println("Conflict");
     System.out.println("A");
-    System.out.println("Hello world");
+    System.out.println("Hello world333");
     System.out.println("BBB");
   }
 }
